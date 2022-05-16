c9b7001242be508fccd8493d33e761a5ad4f33b9
Rasmus Toftdahl Olesen <rtol@demant.com>
2022-05-16 08:50:14 +0200

    Update build.sh

--- image/build.sh
+++ image/build.sh
@@ -5,7 +5,7 @@ echo ------------------
 echo Combining part A and part B
 echo Part A >> out.txt
 echo ------------------ >> out.txt
-cat part_a/out.txt >> out.txt
+cat ../part_a/out.txt >> out.txt
 echo Part B >> out.txt
 echo ------------------ >> out.txt
-cat part_b/out.txt >> out.txt
+cat ../part_b/out.txt >> out.txt
